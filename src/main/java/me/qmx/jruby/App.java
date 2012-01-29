10031199d857acc727620fc67a4b7d24facf6290
Douglas Campos <qmx@qmx.me>
2012-01-29 18:12:09 -0200

    Initial commit

---
+++ src/main/java/me/qmx/jruby/App.java
@@ -0,0 +1,13 @@
+package me.qmx.jruby;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
