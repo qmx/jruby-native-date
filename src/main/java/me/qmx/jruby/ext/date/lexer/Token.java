6fc1ef3411b7dcfe62ce52a5b3e6334f7480d10b
Douglas Campos <qmx@qmx.me>
2012-01-29 20:28:57 -0200

    initial token impl

--- src/main/java/me/qmx/jruby/ext/date/lexer/Token.java
+++ src/main/java/me/qmx/jruby/ext/date/lexer/Token.java
@@ -2,4 +2,16 @@ package me.qmx.jruby.ext.date.lexer;
 
 public class Token {
 
+    private final String value;
+
+    public Token(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "value='" + value + '\'' +
+                '}';
+    }
 }
