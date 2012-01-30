ca71a42bb9a2c9d9d02c5e65d86fdef76463b7e8
Douglas Campos <qmx@qmx.me>
2012-01-30 02:59:36 -0200

    fixing method visibility

--- src/main/java/me/qmx/jruby/ext/date/DateService.java
+++ src/main/java/me/qmx/jruby/ext/date/DateService.java
@@ -4,6 +4,7 @@ import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.Visibility;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.runtime.load.BasicLibraryService;
 
@@ -21,7 +22,7 @@ public class DateService implements BasicLibraryService {
         return true;
     }
 
-    @JRubyMethod(name = "_strptime_i")
+    @JRubyMethod(name = "_strptime_i", visibility = Visibility.PRIVATE)
     public static IRubyObject strptime_i(ThreadContext ctx, IRubyObject self, IRubyObject str, IRubyObject fmt, IRubyObject bag) {
         System.out.println("overriden, forwarding to ruby");
         return self.callMethod(ctx, "_strptime_i_old", new IRubyObject[]{str, fmt, bag});
